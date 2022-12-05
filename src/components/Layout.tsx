a894295b1bffdb791c50f08ef6f71e2f5b101132
Paul Osinowo <Paul>
2022-12-05 21:25:16 +0100

    initialize application

---
+++ src/components/Layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BaseLayout: React.FC = () => {
+  return (
+    <React.Fragment>
+      <main className="container"></main>
+    </React.Fragment>
+  );
+};
+
+export default BaseLayout;
